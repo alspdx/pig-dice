04fec1edad6756477c618d4ab00f7c7f024c7944
Adam Smith and Nathan Barrett <adam@>
2017-11-01 11:33:46 -0700

    add function to add die roll to player turn score, change player and return to 0 if roll is 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,56 @@
-var rollDice = function() {
+function Player(name, turnScore, totalScore) {
+  this.name = name;
+  this.turnScore = turnScore;
+  this.totalScore = totalScore;
+}
+
+var playerOne;
+var playerTwo;
+var allPlayers;
+
+var newGame = function(pOneName, pTwoName) {
+  playerOne = new Player(pOneName, 0, 0);
+  playerTwo = new Player(pTwoName, 0, 0);
+  allPlayers = [playerOne, playerTwo];
+}
+// var allPlayers = [playerOne, playerTwo];
+var currentPlayer = 0;
+
+var rollDie = function() {
   return Math.floor(Math.random() * 6) + 1
 }
+
+
+
+$(function() {
+  $("#player-names").submit(function(event) {
+    event.preventDefault();
+    if ($("#playerOne-name").val() && $("#playerTwo-name").val()) {
+      newGame($("#playerOne-name").val(), $("#playerTwo-name").val());
+      $("#start-screen").slideUp();
+      $("#name-warning").hide();
+    } else {
+      $("#name-warning").show();
+    }
+
+  });
+
+  $("#roll-button").click(function() {
+    var rollDieReturn = rollDie();
+    var playerScore = allPlayers[currentPlayer].turnScore
+    if (rollDieReturn > 1) {
+      playerScore = playerScore + rollDieReturn;
+      allPlayers[currentPlayer].turnScore = playerScore;
+    } else if (rollDieReturn === 1) {
+      playerScore = 0;
+      if (currentPlayer === 0) {
+        currentPlayer = 1
+      } else if (currentPlayer === 1) {
+        currentPlayer = 0
+      };
+    };
+    console.log(currentPlayer + " " + rollDieReturn + " " + playerScore)
+  });
+
+
+});
