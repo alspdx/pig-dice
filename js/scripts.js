0c54dc00862d1b9c16ac2affd5fbbf5021abd4e4
Adam Smith and Nathan Barrett <nathan@>
2017-11-02 14:55:41 -0700

    add evrything else

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,8 @@ var notCurrentPlayer = 1;
 var currentPlayer = 0;
 var playerTurnScore;
 var playerTotalScore;
+var dieAmount;
+
 
 var newGame = function(pOneName, pTwoName) {
   playerOne = new Player(pOneName, 0, 0);
@@ -25,27 +27,32 @@ var rollDie = function() {
 }
 
 var rollTwoDie = function() {
-  return Math.flor(Math.random() * 12) + 2
-}
-
-var rollThreeDie = function() {
-  return Math.flor(Math.random() * 18) + 3
+  return Math.floor(Math.random() * 12) + 2
 }
 
 var rollFourDie = function() {
-  return Math.flor(Math.random() * 24) + 4
+  return Math.floor(Math.random() * 24) + 4
 }
 
 $(function() {
 
   $("#player-names").submit(function(event) {
     event.preventDefault();
+
+
+    if ($("input[type='radio'][name='diceNumber']:checked").val() === "1") {
+      dieAmount = 1;
+    } else if ($("input[type='radio'][name='diceNumber']:checked").val() === "2") {
+      dieAmount = 2;
+    } else if ($("input[type='radio'][name='diceNumber']:checked").val() === "4") {
+      dieAmount = 4;
+    }
     if ($("#playerOne-name").val() && $("#playerTwo-name").val()) {
       newGame($("#playerOne-name").val(), $("#playerTwo-name").val());
       $("#start-screen").slideToggle().css('display', 'flex');
       // $("#play-screen").slideToggle().css('display', 'flex');
       $("#name-warning").hide();
-      $("#current-player-name").text(allPlayers[currentPlayer].name);
+      $(".current-player-name").text(allPlayers[currentPlayer].name);
       $("#player-one-name-input").text(playerOne.name);
       $("#player-two-name-input").text(playerTwo.name);
       $("#player-one-score-output").text(playerOne.totalScore);
@@ -58,19 +65,28 @@ $(function() {
   });
 
   $("#player-ready").click(function() {
-    $("#next-player-name").text(allPlayers[currentPlayer].name);
+    $(".current-player-name").text(allPlayers[currentPlayer].name);
     $("#play-screen").slideToggle().css('display', 'flex');
     $("#next-player-screen").slideToggle();
   });
 
   $("#roll-button").click(function() {
-    var rollDieReturn = rollDie();
+    var rollDieAmount;
+    if (dieAmount === 1) {
+      rollDieReturn = rollDie();
+    } else if (dieAmount === 2) {
+      rollDieReturn = rollTwoDie();
+    } else if (dieAmount === 4) {
+      rollDieReturn = rollFourDie();
+    }
     $("#die-roll-output").text(rollDieReturn);
-    if (rollDieReturn > 1) {
+    if (rollDieReturn > dieAmount) {
       playerTurnScore += rollDieReturn;
       $("#turn-score-output").text(playerTurnScore);
-    } else if (rollDieReturn === 1) {
+    } else if (rollDieReturn === dieAmount) {
       playerTurnScore = 0;
+      rollDieReturn = 0;
+      $("#die-roll-output").text(rollDieReturn);
       $("#turn-score-output").text(playerTurnScore);
       if (currentPlayer === 0) {
         currentPlayer = 1;
@@ -82,32 +98,37 @@ $(function() {
       $("#play-screen").slideUp();
       $("#next-player-screen").slideDown();
     };
-    $("#current-player-name").text(allPlayers[currentPlayer].name);
+    $(".current-player-name").text(allPlayers[currentPlayer].name);
   });
 
   $("#hold-button").click(function(){
-    allPlayers[currentPlayer].totalScore += playerTurnScore;
-    playerTurnScore = 0;
-    $("#player-one-score-output").text(playerOne.totalScore);
-    $("#player-two-score-output").text(playerTwo.totalScore);
-    if (allPlayers[currentPlayer].totalScore < 100) {
-      if (currentPlayer === 0) {
-        currentPlayer = 1;
-        notCurrentPlayer = 0;
-      } else if (currentPlayer === 1) {
-        currentPlayer = 0;
-        notCurrentPlayer = 1;
-      };
-      $("#play-screen").slideUp();
-      $("#next-player-screen").slideDown();
-      $("#current-player-name").text(allPlayers[currentPlayer].name);
-    } else if (allPlayers[currentPlayer].totalScore >= 100) {
-      $("#play-screen").slideUp();
-      $("#winner-screen").slideDown();
-      $("#winner-name-output").text(allPlayers[currentPlayer].name);
-      $("#loser-name-output").text(allPlayers[notCurrentPlayer].name);
-    }
-    $("#turn-score-output").text(playerTurnScore);
-    $("#die-roll-output").text("0");
+    if (playerTurnScore > 0) {
+      allPlayers[currentPlayer].totalScore += playerTurnScore;
+      playerTurnScore = 0;
+      $("#player-one-score-output").text(playerOne.totalScore);
+      $("#player-two-score-output").text(playerTwo.totalScore);
+      if ((allPlayers[currentPlayer].totalScore) < (100 * dieAmount)) {
+        if (currentPlayer === 0) {
+          currentPlayer = 1;
+          notCurrentPlayer = 0;
+        } else if (currentPlayer === 1) {
+          currentPlayer = 0;
+          notCurrentPlayer = 1;
+        };
+        $("#play-screen").slideUp();
+        $("#next-player-screen").slideDown();
+        $(".current-player-name").text(allPlayers[currentPlayer].name);
+      } else if ((allPlayers[currentPlayer].totalScore) >= (100 * dieAmount)) {
+        $("#play-screen").slideUp();
+        $("#winner-screen").slideDown();
+        $("#winner-name-output").text(allPlayers[currentPlayer].name);
+        $(".current-player-name").text(allPlayers[currentPlayer].name);
+        $("#loser-name-output").text(allPlayers[notCurrentPlayer].name);
+      }
+      $("#turn-score-output").text(playerTurnScore);
+      $("#die-roll-output").text("0");
+    } else {
+    alert('It is still your turn.');
+    };
   });
 });
