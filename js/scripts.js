5957009cfd7b0d58ff4e62b598232eca7eee41c9
Adam <alspdx@gmail.com>
2017-11-02 16:18:39 -0700

    refactor several items in js code

--- js/scripts.js
+++ js/scripts.js
@@ -14,50 +14,56 @@ var playerTotalScore;
 var dieAmount;
 
 
-var newGame = function(pOneName, pTwoName) {
+function newGame(pOneName, pTwoName) {
   playerOne = new Player(pOneName, 0, 0);
   playerTwo = new Player(pTwoName, 0, 0);
   allPlayers = [playerOne, playerTwo];
   playerTurnScore = allPlayers[currentPlayer].turnScore;
   playerTotalScore = allPlayers[currentPlayer].totalScore;
+  $("#start-screen").slideToggle().css('display', 'flex');
+  $("#name-warning").hide();
 }
 
-var rollDie = function() {
-  return Math.floor(Math.random() * 6) + 1
+function rollDice(howMany) {
+  return Math.floor(Math.random() * (6 * howMany)) + howMany
 }
 
-var rollTwoDie = function() {
-  return Math.floor(Math.random() * 12) + 2
+function changePlayer() {
+  if (currentPlayer === 0) {
+    currentPlayer = 1;
+    notCurrentPlayer = 0;
+  } else if (currentPlayer === 1) {
+    currentPlayer = 0;
+    notCurrentPlayer = 1;
+  }
 }
 
-var rollFourDie = function() {
-  return Math.floor(Math.random() * 24) + 4
+function nextPlayerScreen() {
+  $("#play-screen").slideToggle().css('display', 'flex');
+  $("#next-player-screen").slideToggle();
+}
+
+function scoreOutputReset() {
+  $("#turn-score-output").text("");
+  $("#die-roll-output").text("");
+  playerTurnScore = 0;
+  rollDiceReturn = 0;
+}
+
+function currentPlayerNameOutput(player) {
+  $(".current-player-name").text(allPlayers[player].name)
 }
 
 $(function() {
-
   $("#player-names").submit(function(event) {
     event.preventDefault();
+    dieAmount = parseInt($("input[type='radio'][name='diceNumber']:checked").val());
 
-
-    if ($("input[type='radio'][name='diceNumber']:checked").val() === "1") {
-      dieAmount = 1;
-    } else if ($("input[type='radio'][name='diceNumber']:checked").val() === "2") {
-      dieAmount = 2;
-    } else if ($("input[type='radio'][name='diceNumber']:checked").val() === "4") {
-      dieAmount = 4;
-    }
     if ($("#playerOne-name").val() && $("#playerTwo-name").val()) {
       newGame($("#playerOne-name").val(), $("#playerTwo-name").val());
-      $("#start-screen").slideToggle().css('display', 'flex');
-      // $("#play-screen").slideToggle().css('display', 'flex');
-      $("#name-warning").hide();
-      $(".current-player-name").text(allPlayers[currentPlayer].name);
+      currentPlayerNameOutput(currentPlayer);
       $("#player-one-name-input").text(playerOne.name);
       $("#player-two-name-input").text(playerTwo.name);
-      $("#player-one-score-output").text(playerOne.totalScore);
-      $("#player-two-score-output").text(playerTwo.totalScore);
-      // $("#play-screen").slideToggle();
       $("#next-player-screen").slideToggle();
     } else {
       $("#name-warning").show();
@@ -65,70 +71,42 @@ $(function() {
   });
 
   $("#player-ready").click(function() {
-    $(".current-player-name").text(allPlayers[currentPlayer].name);
-    $("#play-screen").slideToggle().css('display', 'flex');
-    $("#next-player-screen").slideToggle();
+    currentPlayerNameOutput(currentPlayer);
+    nextPlayerScreen();
   });
 
   $("#roll-button").click(function() {
-    var rollDieAmount;
-    if (dieAmount === 1) {
-      rollDieReturn = rollDie();
-    } else if (dieAmount === 2) {
-      rollDieReturn = rollTwoDie();
-    } else if (dieAmount === 4) {
-      rollDieReturn = rollFourDie();
-    }
-    $("#die-roll-output").text(rollDieReturn);
-    if (rollDieReturn > dieAmount) {
-      playerTurnScore += rollDieReturn;
+    var rollDiceReturn = rollDice(dieAmount);
+    $("#die-roll-output").text(rollDiceReturn);
+    if (rollDiceReturn > dieAmount) {
+      playerTurnScore += rollDiceReturn;
       $("#turn-score-output").text(playerTurnScore);
-    } else if (rollDieReturn === dieAmount) {
-      playerTurnScore = 0;
-      rollDieReturn = 0;
-      $("#die-roll-output").text(rollDieReturn);
-      $("#turn-score-output").text(playerTurnScore);
-      if (currentPlayer === 0) {
-        currentPlayer = 1;
-        notCurrentPlayer = 0;
-      } else if (currentPlayer === 1) {
-        currentPlayer = 0;
-        notCurrentPlayer = 1;
-      };
-      $("#play-screen").slideUp();
-      $("#next-player-screen").slideDown();
+    } else if (rollDiceReturn === dieAmount) {
+      scoreOutputReset();
+      changePlayer();
+      nextPlayerScreen();
     };
-    $(".current-player-name").text(allPlayers[currentPlayer].name);
+    currentPlayerNameOutput(currentPlayer);
   });
 
-  $("#hold-button").click(function(){
+  $("#hold-button").click(function() {
     if (playerTurnScore > 0) {
       allPlayers[currentPlayer].totalScore += playerTurnScore;
-      playerTurnScore = 0;
       $("#player-one-score-output").text(playerOne.totalScore);
       $("#player-two-score-output").text(playerTwo.totalScore);
       if ((allPlayers[currentPlayer].totalScore) < (100 * dieAmount)) {
-        if (currentPlayer === 0) {
-          currentPlayer = 1;
-          notCurrentPlayer = 0;
-        } else if (currentPlayer === 1) {
-          currentPlayer = 0;
-          notCurrentPlayer = 1;
-        };
-        $("#play-screen").slideUp();
-        $("#next-player-screen").slideDown();
-        $(".current-player-name").text(allPlayers[currentPlayer].name);
+        changePlayer();
+        nextPlayerScreen();
       } else if ((allPlayers[currentPlayer].totalScore) >= (100 * dieAmount)) {
         $("#play-screen").slideUp();
         $("#winner-screen").slideDown();
         $("#winner-name-output").text(allPlayers[currentPlayer].name);
-        $(".current-player-name").text(allPlayers[currentPlayer].name);
         $("#loser-name-output").text(allPlayers[notCurrentPlayer].name);
       }
-      $("#turn-score-output").text(playerTurnScore);
-      $("#die-roll-output").text("0");
+      currentPlayerNameOutput(currentPlayer);
+      scoreOutputReset();
     } else {
-    alert('It is still your turn.');
+      alert('Hey! It is still your turn, ' + allPlayers[currentPlayer].name + ", and it will remain your turn until you roll the damn dice!");
     };
   });
 });
